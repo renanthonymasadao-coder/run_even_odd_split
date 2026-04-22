0ed48f6550c690a70afc4a63e095dba53c03c71f
renanthonymasadao-coder <renanthony.masadao@gmail.com>
2026-04-22 19:51:10 +0800

    implementing file reading and number filtering logic for p1

--- number_processor.py
+++ number_processor.py
@@ -1,3 +1,16 @@
 class NumberProcessor:
     def __init__(self, file_name):
         self.file_name = file_name
+
+    def process_numbers(self):
+        with open(self.file_name, 'r') as file:
+            numbers = list(map(int, file.read().split()))
+
+        even_numbers = [n for n in numbers if n % 2 == 0]
+        odd_numbers = [n for n in numbers if n % 2 != 0]
+
+        with open("even.txt", 'w') as even_file:
+            even_file.write(" ".join(map(str, even_numbers)))
+
+        with open("odd.txt", 'w') as odd_file:
+            odd_file.write(" ".join(map(str, odd_numbers)))
